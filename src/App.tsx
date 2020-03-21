63f18a726e9a2159a938f1dacceda6aed1b6a2a1
Benjamin Benoit <>
2020-03-21 17:22:21 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApolloClient, { gql } from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    });
+  },
+});
+
+const GET_USER_INFOS = gql`
+query {
+  viewer {
+    avatarUrl
+  }
+}
+`;
+
+client
+  .query({
+    query: GET_USER_INFOS
+  })
+  .then(result => console.log(result));
 
 function App() {
   return (
