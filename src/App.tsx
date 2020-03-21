71ad8a1b14309898109add21b5c45ad7025f5905
Benjamin Benoit <>
2020-03-21 19:06:43 +0100

    token update

--- src/App.tsx
+++ src/App.tsx
@@ -2,50 +2,87 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
 
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   request: operation => {
     operation.setContext({
       headers: {
-        authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+        Authorization: `Bearer ${process.env.REACT_APP_TOKEN ?process.env.REACT_APP_TOKEN : "14b0765fef7767d62b8184c3fa8c8fa39bc9e4c7"
+      }`,
       },
     });
   },
 });
 
 const GET_USER_INFOS = gql`
-query {
-  viewer {
-    avatarUrl
+  query {
+    viewer {
+      avatarUrl
+    }
   }
-}
 `;
 
-client
-  .query({
-    query: GET_USER_INFOS
-  })
-  .then(result => console.log(result));
+const POPULAR_REPOSITORIES_LIST = gql`
+{
+  search(type: REPOSITORY, first: 10) {
+    repositoryCount
+    edges {
+      node {
+        ... on Repository {
+          name
+          owner {
+            login
+          }
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+client.query({ query: GET_USER_INFOS }).then(result => {
+  console.log(result)
+}).catch(error => {
+  console.log(process.env.REACT_APP_TOKEN);
+  console.log(error)
+} )
+
+
+// client
+//   .query({
+//     query: GET_USER_INFOS
+//   })
+//   .then(result => console.log(result));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
     </div>
+  </ApolloProvider>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="react-scripts" />
+REACT_APP_TOKEN= "14b0765fef7767d62b8184c3fa8c8fa39bc9e4c7"
